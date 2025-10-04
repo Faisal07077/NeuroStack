e5e2125aaaa0e0a387384e6dabb7956c018a437a
Faisal <mohhd.faisal010@gmail.com>
2025-10-04 13:13:49 +0530

    UserService

--- activityService/src/main/java/com/example/activityService/controller/ActivityController.java
+++ activityService/src/main/java/com/example/activityService/controller/ActivityController.java
@@ -1,18 +1,20 @@
 package com.example.activityService.controller;
 
 
+import com.example.activityService.dto.ActivityRequest;
+import com.example.activityService.dto.ActivityResponse;
 import com.example.activityService.service.ActivityService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/activates")
+@RequestMapping("/api/activities")
 @AllArgsConstructor
 public class ActivityController {
     private ActivityService activityService;
     @PostMapping
-    public ResponseEntity<ActivityRespone> tractAcvitity(RequestBody ActivityRequest request){
+    public ResponseEntity <ActivityResponse> trackActivity(@RequestBody ActivityRequest request){
         return ResponseEntity.ok(activityService.trackActivity(request));
     }
 }
